4c0df8da024183b95ce18a2890da7c1479f6bc58
Drew White <jdwhite32@gmail.com>
2021-09-27 15:21:25 -0400

    stupid unused variables

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,6 +1,5 @@
 import React, { useEffect, useState } from 'react';
 import sanityClient from '../client'
-import SocialNav from './SocialNav'
 import BlockContent from "@sanity/block-content-to-react"
 
 export default function About() {
