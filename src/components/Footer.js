7a272355478232737dcab0788a7a2698afcb8295
Drew White <jdwhite32@gmail.com>
2021-10-15 09:50:01 -0400

    footer width and link

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,7 +6,7 @@ import Logo from '../images/drucial_icon.svg'
 export default function footer() {
     return (
         <footer>
-            <img src={Logo} width="52" height="52" alt="drucial.com logo" className="footer-logo"/>
+            <a href="/" className="footer-link"><img src={Logo} width="52" height="52" alt="drucial.com logo" className="footer-logo"/></a>
                 <SocialNav  position={'static'} invert={'invert(100%)'} row={'row'} transform={'none'} margin={0} justify={'space-evenly'}/>
             <div className="footer-info">
                 <p className="copyright">©</p>
