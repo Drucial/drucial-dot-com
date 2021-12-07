2eec11093f9f0adea165aa37cbc663b4da0293e6
Drucial <jdwhite32@gmail.com>
2021-12-07 16:38:26 -0500

    mobilt title font size

---
+++ src/components/NotFound.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default function NotFound() {
+  return (
+    <main>
+      <section>
+        <div className="container-full flex-container flex-center">
+          <h1>404: Page Not Found</h1>
+        </div> 
+      </section>
+    </main>
+  )
+}
